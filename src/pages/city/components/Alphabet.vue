d7bebfd69f223cbee899a9a914cb69db4737f9a5
wangqian <1027700603@qq.com>
2019-01-12 12:13:28 +0800

    feix bug

--- src/pages/city/components/Alphabet.vue
+++ src/pages/city/components/Alphabet.vue
@@ -7,7 +7,7 @@
        :key="item"
        :ref="item"
        @click="handleClick"
-       @touchstart="handleTouchStart"
+       @touchstart.prevent="handleTouchStart"
        @touchmove="handleTouchMove"
        @touchend="handleTouchEnd"
        >{{item}}</li>
